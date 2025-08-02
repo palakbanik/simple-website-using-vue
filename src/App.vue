09646ca20790c2a9c229bbb14b1342a06df9878a
Polok Banik <polokbanik7@gmail.com>
2025-08-03 02:50:01 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import SignUp from "./components/SignUp.vue";
+
+export default {
+  name: "app",
+
+  components: {
+    SignUp,
+  },
+};
+</script>
+
+<template>
+  <SignUp />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
